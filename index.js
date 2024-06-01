aaa7dff4fd4254235e386451b3e890a8a412927a
yohan <yohan@adelios.fr>
2024-06-01 15:32:58 +0200

    style: finish patients list

--- index.js
+++ index.js
@@ -11,35 +11,29 @@ fetch('https://fedskillstest.coalitiontechnologies.workers.dev', {
 })
 	.then((response) => response.json())
 	.then((data) => {
-		const patientsWrapper = document.getElementById('patientsWrapper');
+		const patientsWrapper = document.querySelector('.patientsWrapper');
 		data.forEach((patient) => {
-			console.log('patient', patient);
-			// Crée un nouvel élément pour chaque patient
 			const patientDiv = document.createElement('div');
 			patientDiv.classList.add('singlePatient');
 
 			const img = document.createElement('img');
 			img.classList.add('patientImg');
-			img.src = patient.imageURL || ''; // suppose que l'image est dans patient.imageURL
+			img.src = patient.profile_picture || '';
 			img.alt = '';
 
 			const infoDiv = document.createElement('div');
 
 			const nameP = document.createElement('p');
 			nameP.classList.add('patientName');
-			nameP.textContent = patient.name; // suppose que le nom est dans patient.name
-
+			nameP.textContent = patient.name;
 			const genderP = document.createElement('p');
 			genderP.classList.add('patientGender');
-			genderP.textContent = `${patient.gender}, ${patient.age}`; // suppose que le genre et l'âge sont dans patient.gender et patient.age
-
-			// Ajoute les éléments au div principal
+			genderP.textContent = `${patient.gender}, ${patient.age}`;
 			infoDiv.appendChild(nameP);
 			infoDiv.appendChild(genderP);
 			patientDiv.appendChild(img);
 			patientDiv.appendChild(infoDiv);
 
-			// Ajoute le div patient à la patientsWrapper
 			patientsWrapper.appendChild(patientDiv);
 		});
 	})
