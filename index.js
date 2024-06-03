6a507d06fad823da890a9341f4bc786d4ab78c87
yohan <yohan@adelios.fr>
2024-06-03 19:46:23 +0200

    style: style diagnostic zone

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ fetch('https://fedskillstest.coalitiontechnologies.workers.dev', {
 		jessicaData = data.filter(
 			(patient) => patient.name === 'Jessica Taylor'
 		);
+		/* Display the list of the patient */
 		data.forEach((patient) => {
 			const patientDiv = document.createElement('div');
 			patientDiv.classList.add('singlePatient');
@@ -38,6 +39,41 @@ fetch('https://fedskillstest.coalitiontechnologies.workers.dev', {
 			patientDiv.appendChild(infoDiv);
 
 			patientsWrapper.appendChild(patientDiv);
+		});
+		/* Display the data of Jessica Taylor */
+		// Sélectionne l'élément tbody
+		const tbody = document.getElementById('diagnoticPatientContent');
+
+		// Crée un nouvel élément <tr>
+		jessicaData.forEach((patient, index) => {
+			patient.diagnostic_list.forEach((diagnostic) => {
+				const tr = document.createElement('tr');
+				tr.classList.add('rowContent');
+
+				// Crée et configure les éléments <td>
+				const tdProblem = document.createElement('td');
+				tdProblem.classList.add('problemContent');
+				tdProblem.textContent = diagnostic.name;
+
+				const tdDescription = document.createElement('td');
+				tdDescription.classList.add('descriptionContent');
+				tdDescription.textContent = diagnostic.description;
+
+				const tdStatus = document.createElement('td');
+				tdStatus.classList.add('statusContent');
+				tdStatus.textContent = diagnostic.status;
+
+				// Ajoute les <td> au <tr>
+				tr.appendChild(tdProblem);
+				tr.appendChild(tdDescription);
+				tr.appendChild(tdStatus);
+
+				// Ajoute le <tr> au <tbody>
+				tbody.appendChild(tr);
+			});
+			// patient.diagnotic_list.map(({ name }) => {
+			// 	console.log(name);
+			// });
 		});
 	})
 	.catch((error) => {
